8f86e427d19cb44e22ecdf9f9666e4d169cfebcd
Sytone <sytone@users.noreply.github.com>
2015-03-04 19:53:38 -0800

    Update toasty_notification.php

--- plugins/toasty_notification.php
+++ plugins/toasty_notification.php
@@ -5,19 +5,12 @@
     /**
      * Example webhook format plugin.
      */
-    class BalanceAlertPlugin extends Plugin {
+    class ToastyNotificationPlugin extends Plugin {
         
         public function execute($object) {
             
-	    preg_match('/varasi on (\d+,\d{2}) EUR/', $object->description, $matches);
-	    	    
-	    if (count($matches) == 2) {
-		$balance = $matches[1];
-		send_cross_platform_notification( "Ruokarahatilin saldo", $balance . " EUR" );
-	    } else {
-		send_cross_platform_notification( "Ruokarahatili", "Tarkista saldo" );
-	    }
-	    
+            send_toasty_notification( "BasicTest",  $object->description);
+
 	    return true;
 	    
         }
